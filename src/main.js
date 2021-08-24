18f2ec285bb4d4579dfea47de4f96456b31ff21b
rikako1021 <omote.rikako.19@shizuoka.ac.jp>
2021-08-24 20:47:02 +0900

    ログインテスト

--- src/main.js
+++ src/main.js
@@ -3,6 +3,19 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import firebase from 'firebase'
+
+const config = {
+  apiKey: "AIzaSyDvm_RsTfDS2ek2h-uYZ8n9ShbsyUImPSo",
+    authDomain: "fs-delivery-app.firebaseapp.com",
+    projectId: "fs-delivery-app",
+    storageBucket: "fs-delivery-app.appspot.com",
+    messagingSenderId: "773656066046",
+    appId: "1:773656066046:web:f571c32a5cab7c1f4f72af",
+    measurementId: "G-CTSL664YT8"
+};
+firebase.initializeApp(config);
+firebase.analytics();
 
 Vue.config.productionTip = false
 
