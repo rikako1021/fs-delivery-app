45c9687f00ba59b5a73f3d88b39abc1ae786a44a
Yuki-Takeuchi <55795913+Yuki-Takeuchi@users.noreply.github.com>
2021-08-25 20:05:48 +0900

    修正

--- src/components/shop/myItems.vue
+++ src/components/shop/myItems.vue
@@ -7,6 +7,7 @@
       <div v-for="item in shopItemsArray">
         <div>
           <div class='name'>商品名：{{ item.name }}</div>
+          <div class='name' v-if="shopItemsArray">商品名：{{ item.name }}</div>
           <p v-if="shopItemsArray"> <img src = 'item.img' /> </p>
           <div class='price'>値段：{{ item.price }}</div>
           <div clasee='others'>出品数：{{ item.stock }}<br/>
