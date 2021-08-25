b628720b9c95d69c4335491eb904e982a6a621ad
itykyo <kyo.ssv@gmail.com>
2021-08-25 18:59:20 +0900

    first commit /user/myPage

--- src/components/user/myPage.vue
+++ src/components/user/myPage.vue
@@ -1,31 +1,13 @@
+// マイページ
+
 <template>
     <div id="app">
-        <h1>ユーザホーム</h1>
-        <button @click="returnTop" type="button">ホーム</button>
-        <button type="button">ユーザ情報管理</button>
-        <input type="text" id="valueInput" placeholder="Message">
-        <input type="button" id="updateButton" value="更新"/>
-        <button @click="goItem" type="button">商品検索</button>
-        <button onclick="history.back()">戻る</button>
         <center>
-        <h1>ユーザ情報</h1></center>
-        <body>
-          <div v-for="user in usersArray">
-            {{user.address}}
-            {{user.city}}
-            {{user.mail}}
-            {{user.name}}
-            {{user.prefecture}}
-            {{user.userID}}
-          </div>
-        </body>
-        <table>
-        <tr v-for="user in users" :key="user.id">
-            <td v-text="user.id"></td>
-            <td v-text="user.name"></td>
-            <td v-text="user.email"></td>
-        </tr>
-        </table>
+        <h1>ユーザホーム</h1></center>
+        <button @click="returnTop" type="button">ホーム</button>
+        <button @click="goItem" type="button">商品検索</button>
+        <button type="button">ユーザ情報管理</button>
+        <button onclick="history.back()">戻る</button>
     </div>
 </template>
 
@@ -34,40 +16,9 @@ import firebase from 'firebase'
 
 export default {
   name: 'myPage',
-  // data () {
-  //   return {
-  //     username: '',
-  //     password: ''
-  //   }
-  // },
   data() {
     return {
-      usersArray: [],
     }
-  },
-  created() {
-    const that = this
-    const users = firebase
-      .firestore()
-      .collection("users")
-      .where('userID','==','u00001')
-    users.get().then((snapshot) => {
-      console.log(snapshot);
-      snapshot.forEach((doc) => {
-        const user = doc.data()
-        that.usersArray = [
-          ...that.usersArray,
-          {
-            address: user.address,
-            city: user.city,
-            mail: user.mail,
-            name: user.name,
-            prefecture: user.prefecture,
-            userID: user.userID
-          },
-        ]
-      })
-    })
   },
   methods: {
     returnTop: function () {
