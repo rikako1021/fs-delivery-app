d93fd8c200e2eaa06dd35d24d3329dca7630f372
Yuki-Takeuchi <55795913+Yuki-Takeuchi@users.noreply.github.com>
2021-08-25 18:20:44 +0900

    test

--- src/components/shop/myItems.vue
+++ src/components/shop/myItems.vue
@@ -1,34 +1,19 @@
 <!--商品一覧画面-->
 <template>
   <div>
-    <v-row justify="center" align="center">
-      <v-col
-        v-for="item in shopItemsArray"
-        :key="index"
-        cols="8"
-        sm="6"
-        md="4"
-        lg="3"
-        xl="1"
-      >
-        <v-flex>
-          <v-card class="mt-2 mx-2" height="300px" width="300px" :elevation="5">
-            <v-img
-              class="white--text align-end"
-              height="100px"
-              :src="item.img"
-            ></v-img>
-            <v-card-title class="font-weight-bold">
-              {{ item.name }}
-            </v-card-title>
-            <v-card-subtitle>
-              値段：{{ item.price }}<v-spacer />カテゴリー：{{ item.type }}
-            </v-card-subtitle>
-            <v-card-subtitle>在庫：{{ item.stocks }}</v-card-subtitle>
-          </v-card>
-        </v-flex>
-      </v-col>
-    </v-row>
+    <center>
+      <h1>出品一覧</h1></center>
+    <body>
+      <div v-for="item in shopItemsArray">
+        <div>
+          <div class='name'>商品名：{{ item.name }}</div>
+          <div class='image'>{{ item.img }}</div>
+          <div class='price'>値段：{{ item.price }}</div>
+          <div clasee='others'>出品数：{{ item.stock }}<br/>
+          ジャンル：{{ item.type }}</div>
+        </div>
+      </div>
+    </body>
   </div>
 </template>
 
