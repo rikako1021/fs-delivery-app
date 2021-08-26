e3866b99c249ebd573a7aa39ee4dddb226b8b3ed
itykyo <kyo.ssv@gmail.com>
2021-08-27 02:24:05 +0900

    表示機能の改良 (confirmOrder.vueだけ)

--- src/components/user/confirmOrder.vue
+++ src/components/user/confirmOrder.vue
@@ -4,22 +4,30 @@
     <div id="app">
         <center>
         <h1>注文確認</h1></center>
-        <button @click="returnTop" type="button">ホーム</button><br>
         <button @click="goBack" type="button">買い物を続ける</button><br>
         <body>
+          <center>
+          <h1 v-if="cartItems === null">カートには何も入っていません</h1></center>
           <div v-for="item in cartItems" :key="item.itemID">
             <div>
               <div class='name' v-if="cartItems">商品名：{{ item.name }}</div>
               <p v-if="cartItems"> <img v-bind:src = "item.image"/></p>
               <div class='price'>値段：{{ item.price }}</div>
-              <div class='others'>出品数：{{ item.stock }}<br/>
+              <div class='others'>
               ジャンル：{{ item.type }}<br>
               フードロス[g]：{{ item.weight }}<br></div>
               </div>
             </div>
+
+          <HR></HR>
+
         </body>
-        <div>合計金額: {{ cost }}</div>
-        <button @click="goFinish" type="button">注文確定</button>
+        <center>
+        <h1 v-if="cartItems">合計金額：¥{{ cost }}<br>
+        <button v-if="cartItems" @click="goFinish" class="button">注文確定</button></h1></center>
+      <div　class='backHomeBottom'>
+        <router-link to="/user/userTop">ホームに戻る</router-link>
+      </div>
     </div>
 </template>
 
@@ -36,13 +44,15 @@ export default {
       cost: 0,
     }
   },
-  mounted() {
-    this.cartItems = JSON.parse(localStorage.getItem('obj'))
+  created() {
+    this.cartItems = JSON.parse(sessionStorage.getItem('obj'))
     console.log(this.cartItems)
-    this.cartItems.forEach((item) => {
-      console.log(item.price)
-      this.cost += Number(item.price)
-    })
+    if (this.cartItems != null) {
+      this.cartItems.forEach((item) => {
+        console.log(item.price)
+        this.cost += Number(item.price)
+      })
+    }
   },
   methods: {
     returnTop: function () {
@@ -52,9 +62,119 @@ export default {
       this.$router.push('/user/searchItems')
     },
     goFinish: function () {
-      localStorage.clear()
+      localStorage.setItem('obj', JSON.stringify(
+        this.cartItems
+      ))
+      sessionStorage.clear()
       this.$router.push('/user/finishOrder')
     },
   },
 }
 </script>
+
+<style scoped>
+h1, h2 ,h3{
+  line-height: 55px;
+    font-size: 30px;
+    font-weight: bold;
+    font-family: "Open Sans", sans-serif;
+    text-transform: uppercase;
+    text-align: center;
+    background: $headerbg;
+    margin-top: 10px;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.sale {
+  font-size:20px;
+  margin: auto;
+  padding: 100px;
+  padding-left: 30%;
+  padding-bottom: 0px;
+}
+
+.items{
+  margin-bottom: 10%;
+}
+
+
+.signup {
+  margin-top: 20px;
+
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center
+}
+
+.backHomeBottom{
+border-radius: 40px;
+  padding: 50px;
+  margin: 25px;
+  margin-bottom: 5%;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  border-style: solid;
+  border-color: rgb(18, 95, 63);
+  box-shadow: 0px 0px 5px 0px gray;
+}
+
+.input{
+  font-family: "Roboto", sans-serif;
+  outline: 0;
+  background: #f2f2f2;
+  width: 50%;
+  border: 0;
+  margin: 10px;
+  padding: 15px;
+  box-sizing: border-box;
+  font-size: 14px;
+}
+
+.button{
+font-family: "Roboto", sans-serif;
+  text-transform: uppercase;
+  outline: 0;
+  background: #4CAF50;
+  width: 20%;
+  border: 0;
+  padding: 15px;
+  color: #FFFFFF;
+  font-size: 14px;
+  -webkit-transition: all 0.3 ease;
+  transition: all 0.3 ease;
+  cursor: pointer;
+  margin-bottom: 20px;
+}
+
+.name{
+    font-size: 20px;
+    font-weight: bold;
+    font-family: "Open Sans", sans-serif;
+    text-align: center;
+    background: $headerbg;
+    margin: 0px auto;
+    margin-right: 30%;
+    padding-top:40px;
+    line-height: 200%;
+}
+
+hr{
+  border-top: 3px dashed;
+}
+
+.float_l {
+	float: left;
+}
+
+
+</style>
