fda7c10a7c16f5942ef2d2e8aa02f1e3383ac813
hayato-coder <hayatoto0702@gmail.com>
2021-08-26 14:50:43 +0900

    myshop

--- src/components/shop/myShop.vue
+++ src/components/shop/myShop.vue
@@ -48,3 +48,66 @@ export default {
   }
 }
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-size: 100px;
+  font-weight: bold;
+  color: #42b983;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+input {
+  margin: 10px 0;
+  padding: 10px;
+  width: 130%;
+  font-size: 145%;
+  box-shadow: 0px 0px 5px rgb(56, 56, 56) inset;
+  border: none;
+  border-radius: 6px;
+}
+td {
+    padding: 30px;
+    border-bottom: 1px solid #EEE;
+    padding-right: 80px;
+    border: none;
+}
+tr {
+  margin-right: 10px;
+  text-align: center;
+}
+input {
+  margin: 10px 0;
+  padding: 10px;
+}
+button {
+  font-size: 120%;
+  padding: 30px;
+  margin-left: 50;
+  box-shadow: 1px 1px 5px 3px rgb(173, 173, 173);
+  border: none;
+  width: 50%;
+}
+button h2 {
+  font-family: "Hiragino Maru Gothic ProN W4";
+  font-weight: bold;
+}
+.shopName {
+  font-size: 60px;
+  font-weight: normal;
+}
+.others {
+  font-size: 60px;
+  font-weight: normal;
+}
+</style>
